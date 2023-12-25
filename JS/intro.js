f2c9aca7e57579e3193a39b66095a447f5a5fd79
Abu Salman <121804012+AbuSalman143@users.noreply.github.com>
2023-12-25 22:34:34 +0530

    commit

---
+++ JS/intro.js
@@ -0,0 +1,38 @@
+// var a=10;
+// console.log(a);
+// a=20;
+// console.log(a);
+
+// var a=30;
+// console.log(a);
+
+
+// let b=10;
+// console.log(b);
+// b=20;
+// console.log(b);
+// let b=30;error
+
+
+// const c=10;
+// console.log(c);
+// c=20
+
+
+// let num1=10;
+// let num2=20;
+
+// let sum=num1+num2;
+// console.log(sum);
+
+// // console.log(typeof num1,typeof num2,typeof sum);
+
+let num1=10,num=30;
+ 
+let num2="20";
+
+let sum=num+num2+num1;
+console.log(sum);
+
+console.log(typeof num1,typeof num2,typeof sum);
+
